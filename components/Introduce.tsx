c102b93c05ae4c1197ef516b3332f8daa2af9ae8
JunDemi <jungwook3176@gmail.com>
2023-07-28 14:32:17 +0900

    Feat: 심사위원 커포넌트 추가

---
+++ components/Introduce.tsx
@@ -0,0 +1,25 @@
+export default function Introduce() {
+  return (
+    <div className="bg-[whitesmoke] lg:py-32 py-16 lg:px-24">
+      <div>
+        <h1 className="lg:text-6xl text-4xl lg:text-left text-center text-[#fea116] tracking-wider font-thin">
+          G-Clef
+        </h1>
+        <h2 className="lg:text-2xl text-lg font-bold lg:text-left text-center">
+          Online International Music Competition
+        </h2>
+        <div className="text-left space-y-4 bg-white shadow-xl py-12 px-12 mt-10 lg:mx-0 mx-6 text-slate-800 font-thin lg:text-xl text-sm">
+          <p>It is open to artists of all nationalities.</p>
+          <p>
+            It is an online international music competition where you submit a
+            video and compete.
+          </p>
+          <p>
+            The international music competition is designed to help discover and
+            support great artists on the online stage, a trend of the times.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Introduce from "@/components/Introduce";
+import Jury from "@/components/Jury";
 import { motion } from "framer-motion";
 import Head from "next/head";
 
@@ -12,9 +14,9 @@ export default function Home() {
       <Head>
         <title>G-Clef</title>
       </Head>
-      <div className="pt-32 pb-32 bg-cover bg-no-repeat flex flex-col justify-end items-center"
+      <div className="lg:pt-64 lg:pb-[30rem] py-60 bg-cover bg-no-repeat flex flex-col justify-end items-center"
         style={{backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.9)), url('/bgindex.jpg')"}}>
-          <div className="lg:mt-24 flex justify-center items-center gap-5">
+          <div className="flex justify-center items-center gap-5">
             <span className="lg:w-16 w-8 h-[0.2rem] bg-[#fea116]"></span>
             <h1 className="text-white lg:text-2xl text-base tracking-widest font-thin">The 3rd Online</h1>
             <h1 className="text-white lg:text-5xl text-2xl tracking-widest font-thin">&quot;G-Clef&quot;</h1>
@@ -23,6 +25,8 @@ export default function Home() {
           <h1 className="text-white font-bold lg:text-6xl text-3xl mt-10 text-center tracking-wide">International Music Competition</h1>
           <button className="bg-[#fea116] px-12 py-4 text-white tracking-tight lg:text-xl text-lg mt-10 hover:bg-[#fdad35] transition">APPLY</button>
       </div>
+      <Introduce/>
+      <Jury/>
     </>
   );
 }
